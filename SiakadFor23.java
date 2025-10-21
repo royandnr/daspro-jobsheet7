4623ef5aec6f6dfe8ff133bca109a9cc8f0929bb
Yann <yann@gmail.com>
2025-10-21 20:37:50 +0700

    Modif Percobaan 1: Studi Kasus Nilai Mahasiswa di SIAKAD – Perulangan FOR - untuk soal no 5

--- SiakadFor23.java
+++ SiakadFor23.java
@@ -6,20 +6,34 @@ public class SiakadFor23 {
 
         // deklarasi variabel
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
+        int batasKelulusan = 60;
+
 
         // struktur perulangan for
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
+
+            // menentukan nilai tertinggi dan terendah 
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            // menghitung jumlah mahasiswa lulus dan tidak lulus 
+            if (nilai >= batasKelulusan) {
+                jumlahLulus++;
+            } else {
+                jumlahLulus++;
+            }
         }
         System.out.println("Nilai Tertinggi :" + tertinggi);
         System.out.println("Nilai Terendah :" + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus :" + jumlahLulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus :" + jumlahTidakLulus);
     }
 
 }
